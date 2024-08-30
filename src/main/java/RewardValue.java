4fc24737c0d01f0f6781fe6c947a91cd2def8d91
yuvrajchavale <chawaleyuvraj@gmail.com>
2024-08-30 17:00:32 +0530

    All issues resolves

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,29 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Conversion rate from miles to cash
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_RATE;
+    }
+
+    // Constructor that accepts a miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
